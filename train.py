d3e9621729f981ef2e53bde3005c13b9a535b1c8
fano2458 <fano2458@gmail.com>
2025-02-06 19:26:42 +0500

    training script updated

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from PIL import Image
 from tqdm import tqdm
 from torch.optim.lr_scheduler import CosineAnnealingLR
 import wandb
+import heapq
+import os
 
 
 class ImageCaptionDataset(Dataset):
@@ -18,13 +20,13 @@ class ImageCaptionDataset(Dataset):
 
         with open(data_path, 'r') as f:
             data = json.load(f)
-        self.lables = data['annotations']
+        self.labels = data['annotations']
         
         self.images = []
         self.captions = []
 
         ds_type = "train" if "train" in data_path else "val"
-        for label in self.lables:
+        for label in self.labels:
             image_path = f"data/{ds_type}2017/{label['image_id']:012d}.jpg"
             self.images.append(image_path)
             self.captions.append(label['caption'])
@@ -33,7 +35,6 @@ class ImageCaptionDataset(Dataset):
         return len(self.captions)
     
     def __getitem__(self, idx):
-
         image_path = self.images[idx]
         image = Image.open(image_path).convert("RGB")
         image = self.visual_processor(image)[0,:]
@@ -65,13 +66,15 @@ def train_model():
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-    epochs = 30
+    epochs = 50
     scheduler = CosineAnnealingLR(optimizer, T_max=epochs)
     
+    scaler = torch.cuda.amp.GradScaler()
     wandb.init(project="kaz-clip")
     best_val_loss = float('inf')
     patience = 4
     no_improvement_count = 0
+    top_checkpoints = []
 
     for epoch in range(epochs):
         model.train()
@@ -80,11 +83,13 @@ def train_model():
         for batch_images, batch_text in tqdm(train_dataloader):
             batch_images = batch_images.to(device)
             batch_text = {k: v.to(device) for k, v in batch_text.items()}
-            visual_features, text_features = model(batch_images, batch_text)
-            loss = model.compute_loss(visual_features, text_features)
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            with torch.cuda.amp.autocast():
+                visual_features, text_features = model(batch_images, batch_text)
+                loss = model.compute_loss(visual_features, text_features)
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
             total_train_loss += loss.item()
             train_batches += 1
 
@@ -98,8 +103,9 @@ def train_model():
             for batch_images, batch_text in tqdm(valid_dataloader):
                 batch_images = batch_images.to(device)
                 batch_text = {k: v.to(device) for k, v in batch_text.items()}
-                visual_features, text_features = model(batch_images, batch_text)
-                loss = model.compute_loss(visual_features, text_features)
+                with torch.cuda.amp.autocast():
+                    visual_features, text_features = model(batch_images, batch_text)
+                    loss = model.compute_loss(visual_features, text_features)
                 total_val_loss += loss.item()
                 val_batches += 1
 
@@ -111,9 +117,15 @@ def train_model():
         
         if mean_val_loss < best_val_loss:
             best_val_loss = mean_val_loss
-            torch.save(model.state_dict(), "best_model.pt")
+            best_model_path = f"model_epoch_{epoch+1}_{mean_val_loss:.3f}.pt"
+            torch.save(model.state_dict(), best_model_path)
             print(f"Model saved at epoch {epoch+1} with val loss: {best_val_loss:.4f}")
             no_improvement_count = 0
+            # Maintain top 3 model.state_dicts
+            heapq.heappush(top_checkpoints, (mean_val_loss, best_model_path))
+            if len(top_checkpoints) > 3:
+                _, old_checkpoint = heapq.heappop(top_checkpoints)
+                os.remove(old_checkpoint)
         else:
             no_improvement_count += 1
             if no_improvement_count >= patience:
